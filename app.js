aad3e0461f076cb3dbb3072d354ec8255f8dbc56
hcxie20 <hcxie20@126.com>
2020-02-14 14:44:30 -0800

    add authentication

--- app.js
+++ app.js
@@ -3,16 +3,31 @@ var express = require("express")
     bodyParser = require("body-parser")
     mongoose = require("mongoose")
     methodOverride = require("method-override")
+    passport = require("passport")
+    LocalStrategy = require("passport-local")
+
+    User = require("./models/user")
 
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(methodOverride("_method"))
 
+app.use(require("express-session")({
+    secret: "Secret",
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
-mongoose.connect("mongodb://localhost/restful_blog_app")
+mongoose.connect("mongodb://localhost/blog_app")
 
 var blogSchema = new mongoose.Schema({
     title: String,
@@ -40,11 +55,11 @@ app.get("/blogs", function(req, res){
     })
 })
 
-app.get("/blogs/new", function(req, res){
+app.get("/blogs/new", isLoggedIn, function(req, res){
     res.render("new")
 })
 
-app.post("/blogs", function(req, res){
+app.post("/blogs", isLoggedIn, function(req, res){
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             console.log(err)
@@ -53,6 +68,37 @@ app.post("/blogs", function(req, res){
         }
     })
 })
+
+
+app.get("/blogs/register", function(req, res){
+    res.render("register")
+})
+
+app.post("/blogs/register", function(req, res){
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if (err){
+            res.send(err)
+        }else{
+            console.log("here")
+            passport.authenticate("local")(req, res, function(){
+                console.log("here1")
+                res.send("success")
+            })
+        }
+    })
+})
+
+app.get("/blogs/login", function(req, res){
+    res.render("login")
+})
+
+app.post("/blogs/login", passport.authenticate("local", {
+    successRedirect: "/blogs",
+    failureRedirect: "/blogs/login"
+}), function(req, res){
+
+})
+
 
 app.get("/blogs/:id", function(req, res){
     Blog.findById(req.params.id, function(err, blog){
@@ -93,6 +139,14 @@ app.delete("/blogs/:id", function(req, res){
         }
     })
 })
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next()
+    }else{
+        res.redirect("/blogs/login")
+    }
+}
 
 app.listen(3000, function(){
     console.log("Server")
